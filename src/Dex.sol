ca7b214ffcf4ab8d9eea4c21c092d3b884663e5d
Sori <ljgggg1234@gmail.com>
2024-08-28 16:25:52 +0900

    status commit repository rename

--- src/Dex.sol
+++ src/Dex.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.8.13;
 
 contract Dex {
 	function addLiquidity(uint256 a1, uint256 a2, uint256 a3) returns (uint ret) external {
-		
+		uint ret;
+
+		return ret	
 	}
 	function removeLiquidity(uint a1, uint256 a2, uint256 a3) returns (uint ret) external{
+		uint ret;
 
+		return ret;
 	}
 	function swap(uint a1, uint a2, uint a3) returns(uint ret) external{
+		uint ret;
 
+		return ret;
 	}
 }
