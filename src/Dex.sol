23abd67f728266d637811a0ab57eed122d3e2f41
Sori <ljgggg1234@gmail.com>
2024-08-28 16:15:19 +0900

    openzepplin install before

---
+++ src/Dex.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Dex {
+	function addLiquidity(uint256 a1, uint256 a2, uint256 a3) returns (uint ret) external {
+		
+	}
+	function removeLiquidity(uint a1, uint256 a2, uint256 a3) returns (uint ret) external{
+
+	}
+	function swap(uint a1, uint a2, uint a3) returns(uint ret) external{
+
+	}
+}
